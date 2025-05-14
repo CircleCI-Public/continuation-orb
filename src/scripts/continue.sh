8eee15b3e24a3fd55ea2ee8d0ba20be679da12b6
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-05-14 13:32:34 -0500

    Add retry functionality

--- src/scripts/continue.sh
+++ src/scripts/continue.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
 if [ -z "${CIRCLE_CONTINUATION_KEY}" ]; then
@@ -44,12 +43,30 @@ jq -n \
 
 cat /tmp/circleci/continue_post.json
 
-[ "$(curl \
-        -o /dev/stderr \
-        -w '%{http_code}' \
-        -XPOST \
+MAX_RETRIES=5
+RETRY_COUNT=0
+
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+    RESPONSE=$(curl -i -s \
+        -X POST \
         -H "Content-Type: application/json" \
         -H "Accept: application/json"  \
         --data @/tmp/circleci/continue_post.json \
-        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue")" \
-   -eq 200 ]
+        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue")
+    HTTP_CODE=$(echo "$RESPONSE" | awk '/^HTTP/{code=$2} END{print code}')
+    RETRY_AFTER=$(echo "$RESPONSE" | awk 'BEGIN{IGNORECASE=1} /^Retry-After:/ {print $2}' | tr -d '\r')
+    if [ "$HTTP_CODE" -eq 429 ]; then
+        WAIT=${RETRY_AFTER:-15}
+        echo "Error too many requests. Retrying in $WAIT"
+        sleep "$WAIT"
+        ((RETRY_COUNT++))
+    elif [ "$HTTP_CODE" -eq 200 ]; then
+        exit 0
+    else
+        echo "Error: $HTTP_CODE"
+        exit 1
+    fi
+done
+
+echo "Max retries reached"
+exit 1
--- src/scripts/finish.sh
+++ src/scripts/finish.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if ! which curl > /dev/null; then
     echo "curl is required to use this command"
     exit 1
@@ -14,12 +16,30 @@ JSON_BODY=$( jq -n \
 )
 echo "$JSON_BODY"
 
-[[ $(curl \
-        -o /dev/stderr \
-        -w '%{http_code}' \
-        -XPOST \
+MAX_RETRIES=5
+RETRY_COUNT=0
+
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+    RESPONSE=$(curl -i -s \
+        -X POST \
         -H "Content-Type: application/json" \
         -H "Accept: application/json"  \
         --data "${JSON_BODY}" \
-        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue") \
-   -eq 200 ]]
+        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue")
+    HTTP_CODE=$(echo "$RESPONSE" | awk '/^HTTP/{code=$2} END{print code}')
+    RETRY_AFTER=$(echo "$RESPONSE" | awk 'BEGIN{IGNORECASE=1} /^Retry-After:/ {print $2}' | tr -d '\r')
+    if [ "$HTTP_CODE" -eq 429 ]; then
+        WAIT=${RETRY_AFTER:-15}
+        echo "Error too many requests. Retrying in $WAIT"
+        sleep "$WAIT"
+        ((RETRY_COUNT++))
+    elif [ "$HTTP_CODE" -eq 200 ]; then
+        exit 0
+    else
+        echo "Error: $HTTP_CODE"
+        exit 1
+    fi
+done
+
+echo "Max retries reached"
+exit 1
