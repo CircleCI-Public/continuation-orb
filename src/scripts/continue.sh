f2c57046a33968c493444756734716f5bc9e2137
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-05-16 09:35:27 -0500

    Use retry option

--- src/scripts/continue.sh
+++ src/scripts/continue.sh
@@ -48,30 +48,15 @@ jq -n \
 
 cat /tmp/circleci/continue_post.json
 
-MAX_RETRIES=5
-RETRY_COUNT=0
-
-while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
-    RESPONSE=$(curl -i -s \
-        -X POST \
+[ "$(curl \
+        --retry 5 \
+        --retry-delay 0 \
+        --retry-connrefused \
+        -o /dev/stderr \
+        -w '%{http_code}' \
+        -XPOST \
         -H "Content-Type: application/json" \
         -H "Accept: application/json"  \
         --data @/tmp/circleci/continue_post.json \
-        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue")
-    HTTP_CODE=$(echo "$RESPONSE" | awk '/^HTTP/{code=$2} END{print code}')
-    RETRY_AFTER=$(echo "$RESPONSE" | awk 'BEGIN{IGNORECASE=1} /^Retry-After:/ {print $2}' | tr -d '\r')
-    if [ "$HTTP_CODE" -eq 429 ]; then
-        WAIT=${RETRY_AFTER:-15}
-        echo "Error too many requests. Retrying in $WAIT"
-        sleep "$WAIT"
-        ((RETRY_COUNT++))
-    elif [ "$HTTP_CODE" -eq 200 ]; then
-        exit 0
-    else
-        echo "Error: $HTTP_CODE"
-        exit 1
-    fi
-done
-
-echo "Max retries reached"
-exit 1
+        "https://${CIRCLECI_DOMAIN}/api/v2/pipeline/continue")" \
+   -eq 200 ]
